466100b4de466fd7b6470a49819ea994fed54ee3
久田直治郎 <hisadanaojirou@hisadanaojirou-no-MacBook-Pro.local>
2016-10-30 16:25:03 +0900

    setting_login

--- htdocs/View/User/login.php
+++ htdocs/View/User/login.php
@@ -1,32 +1,53 @@
+<?php
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/Config/loader.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/Model/User.php');
+
+	$loader  = new Loader();
+	$metaData = [
+			'title' =>'ログイン画面',
+			'keywords' =>'ログイン',
+			'description' => 'ログイン画面',
+			'h1' => 'ログイン画面'
+	];
+	if($loader->Request->isPost()){
+		$User = new User();
+		$formData = $_POST['data'];
+		$User->set($formData);
+
+		if($UserArray = $User->authenticate()){
+			$loader->Session->set('user_id',$UserArray['id']);
+			$loader->Session->set('message','ログインしました。');
+			$loader->Request->redirect('User/index');
+		}
+		$errors = $User->ValidationErrors;
+	}
+	$loader->SiteSetting->setMetaData($metaData);
+	include_once($loader->View->getLayout('header'));
+	include_once($loader->View->getElement('nav'));
+?>
+
 <div class="container">
     <div class="row">
     	<div class="col-md-4 col-md-offset-4">
     		<div class="panel panel-default">
 			  	<div class="panel-heading">
-			    	<h3 class="panel-title">Login via site</h3>
+			    	<h3 class="panel-title">ログイン</h3>
 			 	</div>
 			  	<div class="panel-body">
-			    	<form accept-charset="UTF-8" role="form">
+			    	<form  method="POST" action="" role="form">
                     <fieldset>
 			    	  	<div class="form-group">
-			    		    <input class="form-control" placeholder="yourmail@example.com" name="email" type="text">
+			    		    <input class="form-control" placeholder="yourmail@example.com" name="data[User][email]" type="text">
 			    		</div>
 			    		<div class="form-group">
-			    			<input class="form-control" placeholder="Password" name="password" type="password" value="">
+			    			<input class="form-control" placeholder="Password" name="data[User][password]" type="password" value="">
 			    		</div>
-			    		<div class="checkbox">
-			    	    	<label>
-			    	    		<input name="remember" type="checkbox" value="Remember Me"> Remember Me
-			    	    	</label>
-			    	    </div>
 			    		<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
 			    	</fieldset>
 			      	</form>
-                      <hr/>
-                    <center><h4>OR</h4></center>
-                    <input class="btn btn-lg btn-facebook btn-block" type="submit" value="Login via facebook">
 			    </div>
 			</div>
 		</div>
 	</div>
 </div>
+<?php include_once($loader->View->getLayout('footer')); ?>
